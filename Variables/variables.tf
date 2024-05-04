3b13ef0199a230741223c45945350158ffa47cf3
Armandkeza <armandkeza@gmail.com>
2024-05-04 15:12:35 -0400

    Update variables.tf

--- Variables/variables.tf
+++ Variables/variables.tf
@@ -1,19 +1,19 @@
 variable "cust_scope" {
-    default = "/subscriptions/5d7d706c-145c-49b3-a3b3-a2568b15db3a"
+    default = "/subscriptions/yoursubscriptionID"
 }
 
 variable "BU1_scope" {
-    default = "/subscriptions/0adb7bfd-813b-4848-8baa-c78b81122533"
+    default = "/subscriptions/yoursubscriptionID"
 }
 
 variable "management_group_global" {
-    default = "/providers/Microsoft.Management/managementGroups/82cc30e1-db29-426a-a1fc-ed127a04001f"
+    default = "/providers/Microsoft.Management/managementGroups/yourmanagementgroupID"
 }
 
 variable "management_group_application" {
-    default = "/providers/Microsoft.Management/managementGroups/500c0ba4-0f8b-42ae-981e-a596c44b703e"
+    default = "/providers/Microsoft.Management/managementGroups/yourmanagementgroupID"
 }
 
 variable "management_group_platform" {
-    default = "/providers/Microsoft.Management/managementGroups/21cdc292-a2f5-4978-bc95-c58336f8b653"
+    default = "/providers/Microsoft.Management/managementGroups/yourmanagementgroupID"
 }
